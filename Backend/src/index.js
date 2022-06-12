9c5a170619446c66de92ab08082a4b1f036963e7
Seevii16 <sevillanito435@gmail.com>
2022-06-12 19:23:45 +0200

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT;
 
 
 // Conexion con la Base de Datos de Mongo
-mongoose.connect('mongodb+srv://Admin:Admin@cluster0.mixph.mongodb.net/TiendaMangas?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
   const app = express();
   app.use(cors());
   app.use(express.json());
@@ -17,7 +17,7 @@ mongoose.connect('mongodb+srv://Admin:Admin@cluster0.mixph.mongodb.net/TiendaMan
   
   app.use("/", routes);
 
-  app.listen(5000, () => {
+  app.listen(PORT, () => {
     
     console.log("Server has started!");
   });
